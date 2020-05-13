a4481cfdcdce2562aa5c75404cbc7d6fc2504f77
Oskar <oskar.hiden@gmail.com>
2020-05-13 09:15:09 +0200

    2b log prob

--- Lab 3/Lab3.R
+++ Lab 3/Lab3.R
@@ -87,6 +87,7 @@ sort(abs(exp(model$coefficients)-1))
 # 2b
 library(mvtnorm)
 X = as.matrix(ebay[,2:10])
+
 y = ebay[,1]
 prior_cov = t(X)%*%X
 posterior_prob = function(beta, X, y){
@@ -101,6 +102,31 @@ posterior_prob = function(beta, X, y){
   tot_log_like = dim(X)[1]*prior_log_prob + log_like
   return(tot_log_like)
 }
+
+log_faculty = function(y){
+  log_fac = 0
+  if (y != 0) {
+    for (i in 1:y) {
+      log_fac = log_fac + log(i) 
+    }
+  }
+  
+  return(log_fac) 
+}
+
+posterior_prob = function(beta, X, y){
+  log_like=0
+  for (i in 1:dim(X)[1]) {
+    x_b = t(X[i,])%*%beta
+    part = x_b*y[i]+exp(x_b)-log_faculty(y[i])
+    log_like = log_like + part
+  }
+  
+  
+  prior_log_prob = log(dmvnorm(beta, mean = rep(0,nr_param), sigma = prior_cov))
+  tot_log_like = dim(X)[1]*prior_log_prob + log_like
+  return(tot_log_like)
+}
 #startvalues for beta vector
 nr_param = dim(X)[2]
 init_beta = as.vector(rep(0,nr_param))
@@ -109,7 +135,6 @@ result = optim(init_beta, posterior_prob, gr=NULL, X, y, method=c("BFGS"), contr
 beta_hat = result$par
 j_y = -result$hessian
 post_cov = solve(j_y)
-
 
 dmvnorm(init_beta, mean = rep(0,nr_param), sigma = prior_cov)
 t(X[1,])%*%init_beta
